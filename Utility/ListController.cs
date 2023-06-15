63e683c0aec25b11f4637ec52840bd2c4cf51024
ThomasGrant08 <110394805+ThomasGrant08@users.noreply.github.com>
2023-06-15 21:26:14 +0100

    vvh

---
+++ Utility/ListController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace ThomasGrant.Utility
+{
+    public static class ListController
+    {
+        private static Random random = new Random();
+        public static List<IPublishedContent> Shuffle(List<IPublishedContent> content)
+        {
+            int n = content.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = random.Next(n + 1);
+                IPublishedContent value = content[k];
+                content[k] = content[n];
+                content[n] = value;
+            }
+
+            return content;
+        }
+    }
+}
