0bef6413806e8e0d1a460b1f61e96fc6e3cd728d
ThomasGrant08 <thomas.grant@moirae.co.uk>
2023-06-22 17:43:38 +0100

    Fixes

---
+++ Utility/UniqueIDBuilder.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ThomasGrant.Utility
+{
+	public static class UniqueIDBuilder
+	{
+		public static string Generate()
+		{
+			var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+			var pass = string.Empty;
+			Random rnd = new Random();
+
+			for (int i = 0; i < 10; i++)
+			{
+				var c = chars[rnd.Next(chars.Length)];
+				pass = pass + c;
+			}
+
+			return pass;
+
+		}
+	}
+}
